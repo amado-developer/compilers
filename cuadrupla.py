25fa5e024f7dff0dbd30666a76b64a297d648f1f
Mario9810 <32363631+Mcoconan@users.noreply.github.com>
2022-11-14 05:51:21 -0600

    Update cuadrupla.py

--- cuadrupla.py
+++ cuadrupla.py
@@ -1,10 +1,18 @@
+class qBlock:
+    def __init__(self, label, parent = None):
+        self.label = label
+        self.parent = parent
+        self.children = []
+        self.quadruples = []
+        if parent:
+            parent.children.append(self)
+
 class Quadruple:
-  def __init__(self, operator, operand1, operand2, result):
-    self.operator = operator
-    self.operand1 = operand1
-    self.operand2 = operand2
-    self.result = result
-
-
-
-qList = []
+    def __init__(self, operator, operand1, operand2, result):
+        self.operator = operator
+        self.operand1 = operand1
+        self.operand2 = operand2
+        self.result = result
+    def __str__(self):
+        return f'{self.operator} {self.operand1} {self.operand2} {self.result}'
+    
